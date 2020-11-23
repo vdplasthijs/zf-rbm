d37f2b7ae904865566e09c9767745ce101650af7
thijs-bokma <vdplasthijs@gmail.com>
2020-11-23 21:52:49 +0000

    massive update

---
+++ figure_notebooks/plotting_functions.py
@@ -0,0 +1,170 @@
+# @Author: Thijs L van der Plas <thijs>
+# @Date:   2020-11-23
+# @Email:  thijs.vanderplas@dtc.ox.ac.uk
+# @Filename: plotting_functions.py
+# @Last modified by:   thijs
+# @Last modified time: 2020-11-23
+
+
+
+## module with plotting functions
+
+import numpy as np
+# import numpy.matlib
+import h5py
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid1.colorbar import colorbar as mpl_colorbar
+
+import sys,os
+import importlib
+import seaborn as sns
+import pandas as pd
+
+
+def plot_example_hu(ax=None, hu_screenshot_folder='/home/thijs/repos/zf-rbm/figures/HU_screenshots_2020-05-16-0844',
+                    hu_id=86, filename_prefix='screenshot_hu_', y_min=400, y_max=1600, fontsize=10):
+    '''fontsize: 15 for (10, 10) fig, linearly scaling seems fine '''
+                    
+    # screenshot_files = os.listdir(hu_screenshot_folder)
+    filename = filename_prefix + str(hu_id).zfill(3) + '.png'
+    image = mpimg.imread(os.path.join(hu_screenshot_folder, filename))
+    
+    if ax is None:
+        ax = plt.subplot(111)
+    ax.imshow(image[:, y_min:y_max, :])
+    # ax.text(s=f'HU {hu_id}', x=100, y=100, c='white')
+    ax.text(s='Ro', x=25, y=71, c='white', fontdict={'size': fontsize})
+    ax.text(s='C', x=145, y=71, c='white', fontdict={'size': fontsize})
+    ax.text(s='R', x=95, y=1, c='white', fontdict={'size': fontsize, 'va': 'top'})
+    ax.text(s='L', x=95, y=124, c='white', fontdict={'size': fontsize})
+    
+    ax.text(s='Ro', x=25, y=571, c='white', fontdict={'size': fontsize})
+    ax.text(s='C', x=145, y=571, c='white', fontdict={'size': fontsize})
+    ax.text(s='D', x=95, y=501, c='white', fontdict={'size': fontsize, 'va': 'top'})
+    ax.text(s='V', x=95, y=624, c='white', fontdict={'size': fontsize})
+    ax.text(s='0.1 mm', x=1000, y=840, c='white', fontdict={'size': fontsize})
+    
+    ax.set_title(f'HU {hu_id}')
+    ax.axis('off')
+    
+    return ax
+    
+def break_word(word, break_size=25):
+    '''Function that tries to  break up word at first space after given break_size'''
+    if len(word) > break_size:
+        for ii in range(break_size, len(word)):
+            if word[ii] == ' ': # first space encountered
+                word = word[:ii] + '\n' + word[ii:]  #insert break 
+                break 
+    return word
+                
+def plot_info_hu(raster_order, hu_activity, RBM, rec, selection_neurons,
+                 mu=82, n_regions=8, abs_th=0.05, region_absolute=True, save_fig=False):
+    '''Function to plot HU with dynamic activity and top regions 
+    
+    Parameters:
+    -----------
+        raster_order: np array
+            array of length HU, describing the raster mapping (just for name sake)
+        hu_activity: 2D np array 
+            activity matrix (HU x time)
+        RBM: RBM class 
+            RBM to use 
+        rec: zecording object 
+            data set to use 
+        selectoin_neurons: np array 
+            array of indices neurons to use (of rec )
+        mu: int 
+            current HU index
+        n_regions: int 
+            how many regions to plot 
+        abs_th: float 
+            threshold for absolute weights (to count in reigon)
+        region_absolute: bool
+            if True, sort by absolute number of neurons, if False, by relative 
+        save_fig: bool
+            whether to save 
+            
+    Returns:
+    -----------
+        fig: fig handle 
+    '''
+    # plt.rcParams['figure.figsize'] = (10, 5)
+    
+    fig = plt.figure(constrained_layout=False)
+    gs_im = fig.add_gridspec(ncols=1, nrows=1, bottom=0.3, top=0.95, 
+                             hspace=0, wspace=0, left=0.05, right=0.5)  # [1, 2.2, 1.2]
+    gs_regions = fig.add_gridspec(ncols=1, nrows=1, bottom=0.3, top=0.95,
+                                 hspace=0, wspace=0, left=0.85, right=0.95)
+    gs_trace = fig.add_gridspec(ncols=1, nrows=1, bottom=0.05, top=0.18, 
+                                hspace=0, left=0.05, right=0.95)
+
+    freq = 1 / np.mean(np.diff(rec.time))
+    raster_order_reverse = np.zeros_like(raster_order)  # reverse order to find corresponding names 
+    for i_map, map in enumerate(raster_order):
+        raster_order_reverse[map] = i_map
+    
+    ## plot screenshot
+    ax_im = fig.add_subplot(gs_im[0, 0])
+    plot_example_hu(fontsize=7.5, ax=ax_im, hu_id=mu)
+    ax_im.set_title(f'HU (raster plot index): {raster_order_reverse[mu]}, internal index: {mu}',
+                    fontdict={'weight': 'bold'})
+
+    ## plot time trace
+    ax_trace = fig.add_subplot(gs_trace[0, 0])
+    ax_trace.plot(hu_activity[mu, :], c='k', lw=2)
+    ax_trace.set_xticks([x * int(60 * freq) for x in range(9)])  # set time in seconds 
+    ax_trace.set_xticklabels((np.round(ax_trace.get_xticks() / freq)).astype('int'))
+    ax_trace.set_xlabel('Time (s)'); ax_trace.set_ylabel('Activity (a.u.)')
+    ax_trace.spines['top'].set_visible(False)
+    ax_trace.spines['right'].set_visible(False)
+
+    ## plot region occupancy
+    hu_weights = RBM.weights[mu, :]
+    vu_labels = rec.labels[selection_neurons, :]
+    vu_selection = np.abs(hu_weights) >= abs_th  # VUs with weights absolute value geq threshold 
+    vu_selection_labels = vu_labels[vu_selection, :]  # their label 
+    n_vu_per_region_total = np.squeeze(np.array(vu_labels.sum(0)))  # total number of neurons per region 
+    n_vu_per_region = np.squeeze(np.array(vu_selection_labels.sum(0)))  # number of these neurons per region 
+
+    nz_ind = np.where(n_vu_per_region_total)
+    divided_arr = np.zeros_like(n_vu_per_region) + np.nan 
+    divided_arr[nz_ind] = n_vu_per_region[nz_ind] / n_vu_per_region_total[nz_ind]  # divide without zeroes 
+    inds_sorted_div = np.argsort(divided_arr)[::-1] # nan to large to small 
+    if region_absolute:
+        top_region_labels = np.argsort(n_vu_per_region)[::-1]  # sort by top absolute number 
+    else:
+        top_region_labels = inds_sorted_div[np.logical_not(np.isnan(divided_arr[inds_sorted_div]))]  # sort by relative & remove nans 
+    height_bars = divided_arr[top_region_labels[:n_regions]]
+    height_bars_abs = n_vu_per_region[top_region_labels[:n_regions]]
+    with open('/home/thijs/repos/fishualizer/Content/RegionLabels.txt', 'r') as f:
+        all_names = f.readlines()
+    labelnames = np.array([x.strip().rstrip('-') for x in all_names])
+    
+    ax_regions = fig.add_subplot(gs_regions[0, 0])
+    ax_regions_abs = ax_regions.twiny()  # create copy with shared y 
+    ax_regions.barh(y=np.arange(n_regions)- 0.15, color='grey',
+                   width=height_bars * 100, height=0.3)  # multiply with 100 for percetnage
+    ax_regions_abs.barh(y=np.arange(n_regions) + 0.15, color='m',
+                   width=height_bars_abs, height=0.3)
+
+    ax_regions.set_ylabel('Region name')
+    ax_regions.set_xlabel('% of neurons in that region', fontdict={'color': 'k'})
+    ax_regions_abs.set_xlabel('# neurons', fontdict={'color': 'm'})
+    ax_regions_abs.tick_params(axis='x', colors="m")
+    yticklabels = labelnames[top_region_labels[:n_regions]]
+    for iyt, yt in enumerate(yticklabels):
+        new_yt = break_word(word=yt, break_size=30)
+        yticklabels[iyt] = new_yt
+    ax_regions.set_yticks(np.arange(n_regions))
+    ax_regions.set_yticklabels(yticklabels, rotation=0)
+    ax_regions_abs.spines['right'].set_visible(False)
+    ax_regions.spines['right'].set_visible(False)
+
+    if save_fig:
+        plt.savefig(f'/home/thijs/repos/zf-rbm/figures/HU_info_2020-05-16-0844/test_{raster_order_reverse[mu]}.pdf', 
+                 dpi=500, bbox_inches='tight')
+                 
+    return fig
